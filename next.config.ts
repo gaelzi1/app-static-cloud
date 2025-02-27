18c0765e906f84c3d84a125edcdba8a08387a5a9
unknown <mbrito31@alumnos.uaq.mx>
2025-02-27 12:31:22 -0600

    esta es la buena gg

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Sin espacio extra
+  output: "export",
   images: {
-    unoptimized: true, // Necesario para `next export`
+    unoptimized: true,
   },
-  basePath: "/app-static-cloud", // Cambia según la URL de Azure si es necesario
-  assetPrefix: "/app-static-cloud/", // Asegúrate de que sea la misma ruta donde se sirven los archivos estáticos
 };
-
-export default nextConfig;
